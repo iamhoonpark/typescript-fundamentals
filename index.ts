7b73651cc390d71468c1e7647fa4188f5ce3e091
박상훈 <psh995666@gmail.com>
2024-01-26 03:50:05 +0900

    240126 ts 변수선언 1

---
+++ index.ts
@@ -0,0 +1,15 @@
+/**
+ *  변수 선언
+ *  01) 설명: 변수를 선언 시 타입지정 가능
+ *  02) 문법: 변수명 :타입명
+ *  03) 타입: string, number, boolean, bigint, null, undefined,[], {} 등
+ */
+
+// string
+let firstName :string = 'Jake';
+
+// array
+let nameArr :string[] = ['Jake', 'Sanghoon'];
+
+// object
+let ageObj :{ age :number } = { age : 1 };
